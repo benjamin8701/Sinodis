41176c62dc27ee2f5be9fea1526cccc5093b4958
shuqi.chu <shuqi.chu@accenture.com>
2021-09-02 14:47:31 +0800

    Release1 Wave7 Mass Change Account update1

--- aura/CN_Mass_Change_Account_Owner/CN_Mass_Change_Account_OwnerController.js
+++ aura/CN_Mass_Change_Account_Owner/CN_Mass_Change_Account_OwnerController.js
@@ -16,11 +16,14 @@
 		let _titlePromiseHandler = helper.titleHelper(component, titleMap);
 		_titlePromiseHandler.then(function(_returnMap) {
             _returnMap['titleResults'] = $A.get("$Label.c.CN_Account_Mass_Label_Results");
+            _returnMap['titleNotice'] = $A.get("$Label.c.CN_Account_Mass_Label_Notice");
             _returnMap['titleCurrentUser'] = $A.get("$Label.c.CN_Account_Mass_Change_Current");
             _returnMap['titleChangeUser'] = $A.get("$Label.c.CN_Account_Mass_Change_User");
             _returnMap['titleChangeType'] = $A.get("$Label.c.CN_Account_Mass_Change_Type");
             _returnMap['buttonSearch'] = $A.get("$Label.c.CN_Account_Button_Search");
             _returnMap['buttonAssign'] = $A.get("$Label.c.CN_Account_Button_Assign");
+            _returnMap['buttonCancel'] = $A.get("$Label.c.CN_Account_Button_Cancel");
+            _returnMap['buttonProcess'] = $A.get("$Label.c.CN_Account_Button_Process");
             _returnMap['titleCustomerCode'] = $A.get("$Label.c.CN_Account_Mass_Customer_Code");
 			component.set("v.titleMap", _returnMap);
 		});
@@ -58,15 +61,18 @@
         
         
 	},
-        
+    jsPreProcess : function(component, event, helper) {   
+		component.set("v.showSuccess", true);
+    },    
     jsProcess : function(component, event, helper) {
-		component.set("v.showLoading", true);
+        component.set("v.showLoading", true);
 		component.set("v.dataList", [])
 		let _promiseHandler = helper.searchHelper(component, true);
 		_promiseHandler.then(function(_returnValue) {
 			component.set("v.dataList", _returnValue["dataList"]);
             helper.showToast(component, "Success", $A.get("$Label.CN_Mass_Change_Account_Results"), "success");
 			console.log(_returnValue);
+            component.set("v.showSuccess", false);
 		}).catch(function(_error) {
     		console.log(_error);
 		}).finally(function() {
@@ -77,6 +83,9 @@
     onmousehover : function(component, event, helper) {
         console.log('=onmousehover===');
         component.set('v.isTooltip', true);
-    }
+    },
+    jsCloseAlert : function(component, event, helper) {
+		component.set("v.showSuccess", false); 
+	}
 	
 })
